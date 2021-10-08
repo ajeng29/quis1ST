69091bb8bf02ab9bcd86fcc75895613a53774472
ajeng29 <64576935+ajeng29@users.noreply.github.com>
2021-10-08 14:25:24 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -4,12 +4,9 @@
 </head>
 <body>
 	<?php
-	$namaHost = "localhost";
-	$username = "root";
-	$password = "";
-	$database = "projekakhir1";
-	$connect = mysqli_connect($10.0.0.184, $admin, $Teh_manis29, 
-		$kuis1ST);
+	
+	$connect = mysqli_connect(10.0.0.184, admin, Teh_manis29, 
+		kuis1ST);
 	?>
 </body>
 </html>
