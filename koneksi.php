528fef6ec7972bab5e7e455f14659dcc1912383f
ajeng29 <64576935+ajeng29@users.noreply.github.com>
2021-10-08 14:27:45 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -5,8 +5,8 @@
 <body>
 	<?php
 	
-	$connect = mysqli_connect(10.0.0.184, admin, Teh_manis29, 
-		kuis1ST);
+	$connect = mysqli_connect("10.0.0.184", "admin", "Teh_manis29", 
+		"kuis1ST");
 	?>
 </body>
 </html>
